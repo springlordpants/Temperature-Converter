2fd41be1f5cfaf3c9d1f42bd5f897a3193bdf6e6
springlordpants <springlordpants@gmail.com>
2020-08-18 11:14:04 -0700

    Fixing a few styling choices and better organization

--- alert.js
+++ alert.js
@@ -1,1 +1,11 @@
+function toCelsius() {
+  var celsius = (document.getElementById('userInput').value -32) * 5 / 9;
+  celsius = Math.round(celsius);
+  document.getElementById('result').innerHTML = userInput.value + " Fahrenheit is " + celsius + " Celsius";
+}
 
+function toFahrenheit() {
+  var fahrenheit = document.getElementById('userInput').value * 9 / 5 +32;
+  fahrenheit = Math.round(fahrenheit);
+  document.getElementById('result').innerHTML = userInput.value + " Celsius is " + fahrenheit + " Fahrenheit";
+}
